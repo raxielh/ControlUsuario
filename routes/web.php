165e680f4b0d40b3392e80f7fdc694d9bc4f3b20
Rodrigo Garcia <rodrigogarciahoyos@gmail.com>
2017-08-02 01:46:09 -0500

    Se agrego la ruta saludo

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/saludo', function () {
+    return 'Hola';
+});
+
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index');
